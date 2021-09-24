39e4a091c9ea4cca61480f6872d7867214973010
MEY1820 <87822323+MEY1820@users.noreply.github.com>
2021-09-23 21:51:16 -0600

    Add files via upload

---
+++ Figura.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_230921
+{
+    class Figura
+    {
+        // Definir Miembros
+        // Propiedades 
+
+        private int _figuraId;
+        private string _forma;
+        private string _angulo;
+
+        // Propiedades autoimplementadas
+        public string _nombre { get; set; }
+        public string _lados { get; set; }
+
+        // Metodos de propiedades
+        // get-- obtiene el valor almacenado en la propiedad
+        // set-- asignar
+
+        public void setFiguraId(int figuraId)
+        {
+            _figuraId = figuraId;
+        }
+        public int getFiguraId()
+        {
+            return _figuraId;
+        }
+
+        //forma
+        public void setForma(string forma)
+        {
+            _forma = forma;
+        }
+        public string getForma()
+        {
+            return _forma;
+        }
+
+        //_lados
+        public void setlado(string lados)
+        {
+            _lados = lados;
+        }
+        public string getlados()
+        {
+            return _lados;
+        }
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clase_230921
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Instancia de la clase figura 
+
+            Figura figura = new Figura();
+            // MEDIANTE METODOS DE PROPIEDAD
+            // Creamos un objeto figura
+            //SET
+            figura.setFiguraId(1);
+            figura.setForma("Cuadrado");
+
+
+            // Mostrar datos de figura
+            //GET
+            MessageBox.Show(figura.getFiguraId() + figura.getForma());
+
+            //MEDIANTE PROPIEDAD AUTOIMPLEMENTADA
+            //SET
+            figura._lados = "Son 4 lados";
+
+            //GET
+            MessageBox.Show(figura._lados);
+
+        }
+    }
+}
